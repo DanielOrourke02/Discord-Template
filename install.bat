84efc3648391df9b8ab0959eb3a747aec6b82ee1
Daniel Jones <145261080+DanielJones02@users.noreply.github.com>
2024-08-08 22:54:32 +0100

    Updated to py-cord
    Updated old cold now works with py-cord
    
    added descriptions to commands
    
    updated help command

--- install.bat
+++ install.bat
@@ -1,6 +1,3 @@
 @echo off
-
-pip install git+https://github.com/Rapptz/discord.py
 pip install -r requirements.txt
-
 pause
--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-from discord.ext import commands
+from discord.ext import commands, pages
 from colorama import Fore
 from time import ctime
 import discord
 import asyncio
 import json
 
-prefix = '.'  # ignore this the prefix is slash commands anyway
-bot = commands.Bot(command_prefix=prefix, intents=discord.Intents.all(), help_command=None)
+bot = discord.Bot(intents=discord.Intents.all())
 
 # just a variable for formatting
 t = f"{Fore.LIGHTYELLOW_EX}{ctime()}{Fore.RESET}"
@@ -27,9 +26,6 @@ async def on_ready():
     print(f"{t}{Fore.LIGHTBLUE_EX} | Ready and online - {bot.user.display_name}\n{Fore.RESET}")
 
     try:
-        synced = await bot.tree.sync()  # Loads/syncs commands
-        print(f"{t}{Fore.LIGHTBLUE_EX} | {len(synced)} command(s).\n{Fore.RESET}")
-
         guild_count = 0
         for guild in bot.guilds:
             print(f"{Fore.RED}- {guild.id} (name: {guild.name})\n{Fore.RESET}")
@@ -42,60 +38,58 @@ async def on_ready():
         print(e)
 
 
-@bot.tree.command(name="help")
-async def help_command(interaction: discord.Interaction):
-    # Define the list of commands with descriptions
-    commands_list = [
-        {"name": "help", "description": "Displays this message"},
-        {"name": "ping", "description": "Pings the bot"},
-        {"name": "invite", "description": "Add the bot to your server"},
-        {"name": "button", "description": "Displays an example button"},
+@bot.slash_command(name="help", description="Lists all commands")
+@commands.cooldown(1, 3, commands.BucketType.user) # 3 second cooldown 
+async def help_command(ctx: discord.ApplicationContext):
+    command_info = [(command.name, command.description) for command in bot.commands]
+    chunks = [command_info[i:i + 15] for i in range(0, len(command_info), 15)]
+    pages_list = [
+        discord.Embed(
+            title="List of commands:",
+            description="\n".join([f"`/{name}` - {description}" for name, description in chunk]),
+            color=discord.Color.blurple()
+        ) for chunk in chunks
     ]
-
-    # Construct the help message as an embed
-    embed = discord.Embed(title="Bot Commands", color=discord.Colour.blue())
-    for command in commands_list:
-        embed.add_field(name=f"/{command['name']}", value=command['description'], inline=False)
-
-    # Send the help message as an ephemeral response
-    await interaction.response.send_message(embed=embed)
+    paginator = pages.Paginator(pages=pages_list, loop_pages=True, timeout=30)
+    await paginator.respond(ctx.interaction, ephemeral=False)
 
 
-# Simple Test command
-@bot.tree.command(name="ping")
-async def ping(interaction: discord.Interaction):
+@bot.slash_command(name="ping", description="Displays bot's latency")
+@commands.cooldown(1, 3, commands.BucketType.user)
+async def ping(ctx: discord.ApplicationContext):
     embed = discord.Embed(
         title="Pong!",
         description="Pong! {0}".format(round(bot.latency, 1)),
         color=discord.Color.green()
     )
 
-    await interaction.response.send_message(embed=embed, ephemeral=True)
+    await ctx.respond(embed=embed, ephemeral=True)
 
 
-@bot.tree.command(name="invite")
-async def invite(interaction: discord.Interaction):
+@bot.slash_command(name="invite")
+@commands.cooldown(1, 3, commands.BucketType.user)
+async def invite(ctx: discord.ApplicationContext):
     embed = discord.Embed(
         title="Add me!!",
         description=f"[Add me]({bot_invite}) to your server!",
         color=discord.Color.green()
     )
 
-    await interaction.response.send_message(embed=embed, ephemeral=True)
+    await ctx.respond(embed=embed, ephemeral=True)
 
 
+async def ticket_button_callback(ctx):
+    await ctx.respond(f"Thanks for clicking me!", ephemeral=True)
 
-async def ticket_button_callback(interaction):
-    await interaction.response.send_message(f"Thanks for clicking me!", ephemeral=True)
 
-
-@bot.tree.command(name="button")
-async def button_command(interaction: discord.Interaction):  # Corrected parameter name
+@bot.slash_command(name="button", description="Create an example button")
+@commands.cooldown(1, 3, commands.BucketType.user)
+async def button_command(ctx: discord.ApplicationContext):  # Corrected parameter name
     try:
         view = discord.ui.View()
         button = discord.ui.Button(label="Create Ticket", style=discord.ButtonStyle.green)
 
-        button.callback = ticket_button_callback
+        button.callback = ticket_button_callback(ctx)
         view.add_item(button)
 
         # Create an embed
@@ -106,15 +100,8 @@ async def button_command(interaction: discord.Interaction):  # Corrected paramet
         )
 
         # Send a message with the button and attach the view
-        await interaction.response.send_message(embed=embed, view=view)
+        await ctx.respond(embed=embed, view=view)
     except Exception as e:
         print(e)
 
-
-# Use an asynchronous function to run the setup and the bot
-async def run_bot():
-    await bot.start(bot_token)
-
-
-# Run the bot using the asynchronous function
-asyncio.run(run_bot())
+bot.run(bot_token)
